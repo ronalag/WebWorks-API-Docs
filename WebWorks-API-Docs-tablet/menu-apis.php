063b7620caf8f213e05becbdb7236500ae8cdd12
Henry Aghaulor <haghaulor@rim.com>
2012-04-13 16:36:00 -0400

    Updated JSDocs to latest build

---
+++ WebWorks-API-Docs-tablet/menu-apis.php
@@ -0,0 +1,637 @@
+
+
+
+<?php 
+    $menu = array(
+
+    
+        
+            
+            
+array('title'=>'Advertising','link'=>'/html5/apis/blackberry.advertising.Banner.html','id'=>'Advertising','children'=>array(
+    array('title'=>'Banner','link'=>'/html5/apis/blackberry.advertising.Banner.html','id'=>'Banner','children'=>array())
+            
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'BBM','link'=>'/html5/apis/blackberry.bbm.platform.html','id'=>'BBM','children'=>array(
+    array('title'=>'BBM Platform','link'=>'/html5/apis/blackberry.bbm.platform.html','id'=>'BBM Platform','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Connections','link'=>'/html5/apis/blackberry.bbm.platform.io.html','id'=>'Connections','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Current User','link'=>'/html5/apis/blackberry.bbm.platform.self.html','id'=>'Current User','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Other Users','link'=>'/html5/apis/blackberry.bbm.platform.users.html','id'=>'Other Users','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Profile Box','link'=>'/html5/apis/blackberry.bbm.platform.self.profilebox.html','id'=>'Profile Box','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Settings','link'=>'/html5/apis/blackberry.bbm.platform.settings.html','id'=>'Settings','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Cache','link'=>'/html5/apis/blackberry.widgetcache.CacheInformation.html','id'=>'Cache','children'=>array(
+    array('title'=>'CacheInformation','link'=>'/html5/apis/blackberry.widgetcache.CacheInformation.html','id'=>'CacheInformation','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 ApplicationCache','link'=>'/html5/apis/ApplicationCache.html','id'=>'HTML5 ApplicationCache','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'WidgetCache','link'=>'/html5/apis/blackberry.widgetcache.html','id'=>'WidgetCache','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Data Storage','link'=>'/html5/apis/Database.html','id'=>'Data Storage','children'=>array(
+    array('title'=>'HTML5 Database','link'=>'/html5/apis/Database.html','id'=>'HTML5 Database','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Local Storage','link'=>'/html5/apis/localStorage.html','id'=>'HTML5 Local Storage','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 SQLError','link'=>'/html5/apis/SQLError.html','id'=>'HTML5 SQLError','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 SQLResultSet','link'=>'/html5/apis/SQLResultSet.html','id'=>'HTML5 SQLResultSet','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 SQLResultSetRowList','link'=>'/html5/apis/SQLResultSetRowList.html','id'=>'HTML5 SQLResultSetRowList','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 SQLTransaction','link'=>'/html5/apis/SQLTransaction.html','id'=>'HTML5 SQLTransaction','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Device Motion','link'=>'/html5/apis/Acceleration.html','id'=>'Device Motion','children'=>array(
+    array('title'=>'HTML5 Acceleration','link'=>'/html5/apis/Acceleration.html','id'=>'HTML5 Acceleration','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Device Motion Event','link'=>'/html5/apis/DeviceMotionEvent.html','id'=>'HTML5 Device Motion Event','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'GPS','link'=>'/html5/apis/Coordinates.html','id'=>'GPS','children'=>array(
+    array('title'=>'HTML5 Coordinates','link'=>'/html5/apis/Coordinates.html','id'=>'HTML5 Coordinates','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Geolocation','link'=>'/html5/apis/navigator.geolocation.html','id'=>'HTML5 Geolocation','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Position','link'=>'/html5/apis/Position.html','id'=>'HTML5 Position','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 PositionError','link'=>'/html5/apis/PositionError.html','id'=>'HTML5 PositionError','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 PositionOptions','link'=>'/html5/apis/PositionOptions.html','id'=>'HTML5 PositionOptions','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Identity','link'=>'/html5/apis/blackberry.identity.html','id'=>'Identity','children'=>array(
+    array('title'=>'Identity','link'=>'/html5/apis/blackberry.identity.html','id'=>'Identity','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Phone','link'=>'/html5/apis/blackberry.identity.phone.html','id'=>'Phone','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Service','link'=>'/html5/apis/blackberry.identity.Service.html','id'=>'Service','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Transport','link'=>'/html5/apis/blackberry.identity.Transport.html','id'=>'Transport','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Invoke','link'=>'/html5/apis/blackberry.invoke.AddressBookArguments.html','id'=>'Invoke','children'=>array(
+    array('title'=>'AddressBookArguments','link'=>'/html5/apis/blackberry.invoke.AddressBookArguments.html','id'=>'AddressBookArguments','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'BrowserArguments','link'=>'/html5/apis/blackberry.invoke.BrowserArguments.html','id'=>'BrowserArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'CalendarArguments','link'=>'/html5/apis/blackberry.invoke.CalendarArguments.html','id'=>'CalendarArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'CameraArguments','link'=>'/html5/apis/blackberry.invoke.CameraArguments.html','id'=>'CameraArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Invoke','link'=>'/html5/apis/blackberry.invoke.html','id'=>'Invoke','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'JavaArguments','link'=>'/html5/apis/blackberry.invoke.JavaArguments.html','id'=>'JavaArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'MapsArguments','link'=>'/html5/apis/blackberry.invoke.MapsArguments.html','id'=>'MapsArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'MemoArguments','link'=>'/html5/apis/blackberry.invoke.MemoArguments.html','id'=>'MemoArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'MessageArguments','link'=>'/html5/apis/blackberry.invoke.MessageArguments.html','id'=>'MessageArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'PhoneArguments','link'=>'/html5/apis/blackberry.invoke.PhoneArguments.html','id'=>'PhoneArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'SearchArguments','link'=>'/html5/apis/blackberry.invoke.SearchArguments.html','id'=>'SearchArguments','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'TaskArguments','link'=>'/html5/apis/blackberry.invoke.TaskArguments.html','id'=>'TaskArguments','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'IO','link'=>'/html5/apis/blackberry.io.dir.html','id'=>'IO','children'=>array(
+    array('title'=>'Directory','link'=>'/html5/apis/blackberry.io.dir.html','id'=>'Directory','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'File','link'=>'/html5/apis/blackberry.io.file.html','id'=>'File','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'FileProperties','link'=>'/html5/apis/blackberry.io.file.FileProperties.html','id'=>'FileProperties','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Media','link'=>'/html5/apis/blackberry.audio.html','id'=>'Media','children'=>array(
+    array('title'=>'Audio','link'=>'/html5/apis/blackberry.audio.html','id'=>'Audio','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Audio Player','link'=>'/html5/apis/blackberry.audio.Player.html','id'=>'Audio Player','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Camera','link'=>'/html5/apis/blackberry.media.camera.html','id'=>'Camera','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Audio','link'=>'/html5/apis/HTMLAudioElement.html','id'=>'HTML5 Audio','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Video','link'=>'/html5/apis/HTMLVideoElement.html','id'=>'HTML5 Video','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Microphone','link'=>'/html5/apis/blackberry.media.microphone.html','id'=>'Microphone','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Message','link'=>'/html5/apis/blackberry.message.Message.html','id'=>'Message','children'=>array(
+    array('title'=>'Message','link'=>'/html5/apis/blackberry.message.Message.html','id'=>'Message','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'SMS','link'=>'/html5/apis/blackberry.message.sms.html','id'=>'SMS','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Payment','link'=>'/html5/apis/blackberry.payment.html','id'=>'Payment','children'=>array(
+    array('title'=>'Payment','link'=>'/html5/apis/blackberry.payment.html','id'=>'Payment','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Purchase','link'=>'/html5/apis/Purchase.html','id'=>'Purchase','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Phone','link'=>'/html5/apis/blackberry.phone.Phone.Call.html','id'=>'Phone','children'=>array(
+    array('title'=>'Call','link'=>'/html5/apis/blackberry.phone.Phone.Call.html','id'=>'Call','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'CallLog','link'=>'/html5/apis/blackberry.phone.PhoneLogs.CallLog.html','id'=>'CallLog','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Find FilterExpression','link'=>'/html5/apis/blackberry.phone.Find.FilterExpression.html','id'=>'Find FilterExpression','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Phone','link'=>'/html5/apis/blackberry.phone.Phone.html','id'=>'Phone','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'PhoneLogs','link'=>'/html5/apis/blackberry.phone.PhoneLogs.html','id'=>'PhoneLogs','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'PIM','link'=>'/html5/apis/blackberry.pim.Address.html','id'=>'PIM','children'=>array(
+    array('title'=>'Address','link'=>'/html5/apis/blackberry.pim.Address.html','id'=>'Address','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Appointment','link'=>'/html5/apis/blackberry.pim.Appointment.html','id'=>'Appointment','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Attendee','link'=>'/html5/apis/blackberry.pim.Attendee.html','id'=>'Attendee','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Category','link'=>'/html5/apis/blackberry.pim.category.html','id'=>'Category','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Contact','link'=>'/html5/apis/blackberry.pim.Contact.html','id'=>'Contact','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'FilterExpression','link'=>'/html5/apis/blackberry.find.FilterExpression.html','id'=>'FilterExpression','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Memo','link'=>'/html5/apis/blackberry.pim.Memo.html','id'=>'Memo','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Recurrence','link'=>'/html5/apis/blackberry.pim.Recurrence.html','id'=>'Recurrence','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Reminder','link'=>'/html5/apis/blackberry.pim.Reminder.html','id'=>'Reminder','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Task','link'=>'/html5/apis/blackberry.pim.Task.html','id'=>'Task','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Push','link'=>'/html5/apis/blackberry.push.Data.html','id'=>'Push','children'=>array(
+    array('title'=>'Data','link'=>'/html5/apis/blackberry.push.Data.html','id'=>'Data','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Push','link'=>'/html5/apis/blackberry.push.html','id'=>'Push','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'System','link'=>'/html5/apis/blackberry.app.html','id'=>'System','children'=>array(
+    array('title'=>'Application','link'=>'/html5/apis/blackberry.app.html','id'=>'Application','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Application Event','link'=>'/html5/apis/blackberry.app.event.html','id'=>'Application Event','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Web Workers','link'=>'/html5/apis/Worker.html','id'=>'HTML5 Web Workers','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Network','link'=>'/html5/apis/blackberry.html','id'=>'Network','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'System','link'=>'/html5/apis/blackberry.system.html','id'=>'System','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'System Event','link'=>'/html5/apis/blackberry.system.event.html','id'=>'System Event','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Touch','link'=>'/html5/apis/Element.html','id'=>'Touch','children'=>array(
+    array('title'=>'HTML5 Element Touch Events','link'=>'/html5/apis/Element.html','id'=>'HTML5 Element Touch Events','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Touch Event','link'=>'/html5/apis/TouchEvent.html','id'=>'HTML5 Touch Event','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'User Interface','link'=>'/html5/apis/blackberry.ui.dialog.html','id'=>'User Interface','children'=>array(
+    array('title'=>'Dialog','link'=>'/html5/apis/blackberry.ui.dialog.html','id'=>'Dialog','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Focus','link'=>'/html5/apis/blackberry.focus.html','id'=>'Focus','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 2D Canvas Context','link'=>'/html5/apis/CanvasRenderingContext2D.html','id'=>'HTML5 2D Canvas Context','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 Canvas','link'=>'/html5/apis/canvas.html','id'=>'HTML5 Canvas','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'HTML5 WebGL Canvas Context','link'=>'/html5/apis/WebGLRenderingContext.html','id'=>'HTML5 WebGL Canvas Context','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'Menu','link'=>'/html5/apis/blackberry.ui.menu.html','id'=>'Menu','children'=>array())
+        
+    
+
+    
+            
+    ,array('title'=>'MenuItem','link'=>'/html5/apis/blackberry.ui.menu.MenuItem.html','id'=>'MenuItem','children'=>array())
+        
+    
+
+    
+        
+            
+)),
+            
+            
+array('title'=>'Utilities','link'=>'/html5/apis/blackberry.utils.URL.html','id'=>'Utilities','children'=>array(
+    array('title'=>'URL','link'=>'/html5/apis/blackberry.utils.URL.html','id'=>'URL','children'=>array())
+            
+        
+    
+
+    
+            
+    ,array('title'=>'Utils','link'=>'/html5/apis/blackberry.utils.html','id'=>'Utils','children'=>array())
+        
+    
+
+))
+);
+?>
